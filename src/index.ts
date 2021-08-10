65598cf3c1dc0122e8b41cba69aa0c3295fb6c6b
TheBoringDude <iamcoderx@gmail.com>
2021-08-10 09:18:01 +0800

    start

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ message: "Hello World" }),
+  };
+};
+
+export { handler };
